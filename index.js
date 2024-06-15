8f9f3a15a6b5fd47bf81bce1056aa70d16931bce
AmnaKhalil96 <anna963621@gmail.com>
2024-06-15 15:13:06 +0500

    update

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+//Printing a Welcome Message
+console.log(chalk.bold.rgb(204, 204, 204)(`\n  \t\t <<<================================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<=========>>>    ${chalk.magentaBright.bold('  Welcome To \"SAM\" - CLI Number Guessing Game')}         <<<=========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`\t\t <<<================================================>>>\n`));
 // 1) Computer will generate a random number
 // 2) User input for guessing number
 // 3) Compare user input with computer generated number and show result
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-console.log("Welcome to number Guessing game");
-const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1-10:",
+// Function to run the number guessing game
+async function main() {
+    let runContinue = true;
+    while (runContinue) {
+        const randomNumber = Math.floor(Math.random() * 10 + 1);
+        console.log(chalk.blackBright("Welcome to number Guessing game"));
+        const answers = await inquirer.prompt([
+            {
+                name: "userGuessedNumber",
+                type: "number",
+                message: chalk.blueBright("Please guess a number between 1-10:"),
+                validate: (input) => {
+                    if (input < 1 || input > 10) {
+                        return chalk.red("Please enter a number between 1 and 10.");
+                    }
+                    return true;
+                },
+            },
+        ]);
+        if (answers.userGuessedNumber === randomNumber) {
+            console.log(chalk.green("Congratulations! you guess right number."));
+        }
+        else {
+            console.log(chalk.redBright("You guessed wrong number"));
+        }
+        const { confirm } = await inquirer.prompt({
+            type: "confirm",
+            name: "confirm",
+            message: chalk.magentaBright("Do you want to play again?"),
+            default: false,
+        });
+        runContinue = confirm;
     }
-]);
-if (answers.userGuessedNumber === randomNumber) {
-    console.log("Congratulations! you guess right number.");
+    console.log(chalk.yellow("\n\t******************************************************\t"));
+    console.log(chalk.greenBright("\n\tThank you for using the SAM Number Guessing Game!\t"));
 }
-else {
-    console.log("You guessed wrong number");
-}
+main();
--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+//Printing a Welcome Message
+console.log(chalk.bold.rgb(204, 204, 204)(`\n  \t\t <<<================================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<=========>>>    ${chalk.magentaBright.bold('  Welcome To \"SAM\" - CLI Number Guessing Game')}         <<<=========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`\t\t <<<================================================>>>\n`));
 
 // 1) Computer will generate a random number
 
@@ -8,9 +15,16 @@ import inquirer from "inquirer";
 
 // 3) Compare user input with computer generated number and show result
 
+// Function to run the number guessing game
+ async function main() {
+     let runContinue = true;
+   
+     while(runContinue){
+  
+
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
-console.log("Welcome to number Guessing game");
+console.log(chalk.blackBright("Welcome to number Guessing game"));
 
 
 const answers = await inquirer.prompt(
@@ -18,16 +32,41 @@ const answers = await inquirer.prompt(
         {
             name: "userGuessedNumber",
             type: "number",
-            message: "Please guess a number between 1-10:",
-        }
+            message: chalk.blueBright("Please guess a number between 1-10:"),
+            validate: (input) =>{
+                if (input < 1 || input > 10){
+                  return chalk.red("Please enter a number between 1 and 10.")
+                }
+                return true;
+              }, 
+        },
     ]
 );
 
 if(answers.userGuessedNumber === randomNumber){
-console.log("Congratulations! you guess right number.");
+console.log(chalk.green("Congratulations! you guess right number."));
 
 }
 else{
-console.log("You guessed wrong number");
+console.log(chalk.redBright("You guessed wrong number"));
 
 }
+
+const {confirm} =await inquirer.prompt({
+    type: "confirm",
+    name: "confirm",
+    message: chalk.magentaBright("Do you want to play again?"),
+    default: false,
+  });
+  
+   runContinue = confirm;
+    }
+  console.log(chalk.yellow("\n\t******************************************************\t"));
+
+  console.log(chalk.greenBright("\n\tThank you for using the SAM Number Guessing Game!\t"));
+  
+  }
+main()
+
+
+
